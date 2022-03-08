be6ac339f6b94c8a627371e35ad62f557656439d
Julia Gonzalez <vanesa_garro@hotmail.com>
2022-03-08 15:06:28 -0300

    espera implicita en alert

--- src/test/java/DemoBlaze.java
+++ src/test/java/DemoBlaze.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -27,27 +26,20 @@ public class DemoBlaze {
         // Primer producto
         WebDriverWait wait = new WebDriverWait(driver, 7);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.card-img-top"))).click();
-        //driver.findElement().click();
 
 
         // Laptop y precio
         String laptop;
         String precio;
-
         laptop = driver.findElement(By.cssSelector("h2.name")).getText();
-
         precio = driver.findElement((By.cssSelector("h3.price-container"))).getText();
-
         System.out.println("laptop: " + laptop + "Precio" + precio);
 
 
         //Agregar al carrito
         driver.findElement(By.linkText("Add to cart")).click();
 
-
-        //Creo alert
-        //Thread.sleep(2000);
-
+        //Alert
         try {
 
             WebDriverWait alertWait = new WebDriverWait(driver, 3);
@@ -58,7 +50,7 @@ public class DemoBlaze {
             alert.accept();
 
         } catch (Exception e) {
-            //
+            System.out.println("No paso");
         }
         driver.quit();
     }
